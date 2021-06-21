965c7d899390194b5ac335db2b5bf3bba646a237
SrujanGuptha <57907606+srujanguptha@users.noreply.github.com>
2021-06-21 14:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function add() {
+
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+
+
+    var result = num1 + num2
+    alert(result)
+
+}
+function sub() {
+
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+
+  var result = num1 - num2
+  alert(result)
+}
+function mul() {
+
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+
+  var result = num1 * num2
+  alert(result)
+}
+function div() {
+
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+
+  var result = num1 / num2
+  alert(result)
+}
+function rem() {
+
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+
+  var result = num1 % num2
+  alert(result)
+}
